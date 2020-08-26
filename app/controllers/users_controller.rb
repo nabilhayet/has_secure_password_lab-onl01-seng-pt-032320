cf0a12d70cd4a51564420e3bb8d951b27ea9d920
nabilhayet <nabil4457@yahoo.com>
2020-08-25 20:52:44 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+    def new 
+        user = User.new 
+    end 
+
+    def create
+        User.create(user_params)
+    end
+
+    def home
+    end 
+     
+      private
+     
+      def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+      end
+end 

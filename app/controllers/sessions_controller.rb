1f8a21e4c97c0076ccb90e0c9ce14937f6baed2d
nabilhayet <nabil4457@yahoo.com>
2020-08-25 22:50:39 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
 
  def new 
- user = User.new 
+
  end 
     
  def create
   @user = User.find_by(name: params[:user][:name])
-   if !@user.authenticate(password: params[:user][:password])
+   if !@user.authenticate(params[:user][:password])
 
       redirect_to "/login"
    else 
